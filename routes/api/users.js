9863d9220f5787531624ac06f5e5ba4c1edea2c9
Riley-Brown <riley@riley.gg>
2018-11-21 01:38:12 -0500

    added register users route and connected to mongo

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,5 +1,10 @@
 const express = require("express");
 const router = express.Router();
+const gravatar = require("gravatar");
+const bcrypt = require("bcryptjs");
+
+// user model
+const User = require("../../models/User");
 
 // @route   GET api/users/test
 // @desc    Test users route
@@ -8,4 +13,39 @@ router.get("/test", (req, res) => {
   res.json({ message: "working" });
 });
 
+// @route   GET api/users/register
+// @desc    Register new user
+// @access  Public
+router.post("/register", (req, res) => {
+  User.findOne({
+    email: req.body.email
+  }).then(user => {
+    if (user) {
+      return res.status(400).json({ email: "That email already exists" });
+    } else {
+      const avatar = gravatar.url(req.body.email, {
+        s: "200",
+        r: "pg",
+        d: "mm"
+      });
+      const newUser = new User({
+        name: req.body.name,
+        email: req.body.email,
+        avatar: avatar,
+        password: req.body.password
+      });
+
+      bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(newUser.password, salt, (err, hash) => {
+          if (err) throw err;
+          newUser.password = hash;
+          newUser
+            .save()
+            .then(user => res.json(user))
+            .catch(err => console.log(err));
+        });
+      });
+    }
+  });
+});
 module.exports = router;
