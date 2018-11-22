3e7db560ecb0dbdc34ff7012f4ec092d3937c0ca
Riley-Brown <riley@riley.gg>
2018-11-22 17:08:52 -0500

    Added profile route with validation

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,5 +1,12 @@
 const express = require("express");
 const router = express.Router();
+const mongoose = require("mongoose");
+const passport = require("passport");
+
+// Load profile model
+const Profile = require("../../models/Profile");
+// Load user profile
+const User = require("../../models/User");
 
 // @route   GET api/profile/test
 // @desc    Test profile route
@@ -8,4 +15,24 @@ router.get("/test", (req, res) => {
   res.json({ message: "working" });
 });
 
+// @route   GET api/profile
+// @desc    Get current users profile
+// @access  Privtae
+router.get(
+  "/",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    const errors = {};
+
+    Profile.findOne({ user: req.user.id })
+      .then(profile => {
+        if (!profile) {
+          errors.noprofile = "There is no profile for this user";
+          return res.status(404).json(errors);
+        }
+        res.json(profile);
+      })
+      .catch(err => res.status(404).json(err));
+  }
+);
 module.exports = router;
