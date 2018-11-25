04644dc64319a2c4433831d7f4597a5c7d454344
Riley-Brown <riley@riley.gg>
2018-11-25 17:22:43 -0500

    added action and reducer to handle user login token from backend api

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -1,6 +1,9 @@
-// import types
 import axios from "axios";
-import { GET_ERRORS } from "./types";
+import setAuthToken from "../utils/setAuthToken";
+import jwt_decode from "jwt-decode";
+
+// import types
+import { GET_ERRORS, SET_CURRENT_USER } from "./types";
 
 // register user
 export const registerUser = (userData, history) => dispatch => {
@@ -13,4 +16,36 @@ export const registerUser = (userData, history) => dispatch => {
         payload: err.response.data
       })
     );
+};
+
+// login - get user auth token
+export const loginUser = userData => dispatch => {
+  axios
+    .post("/api/users/login", userData)
+    .then(res => {
+      // save token to local storage
+      const { token } = res.data;
+      localStorage.setItem("jwtToken", token);
+
+      // set token to auth header
+      setAuthToken(token);
+      // decode token to get user data
+      const decoded = jwt_decode(token);
+      // set current user
+      dispatch(setCurrentUser(decoded));
+    })
+    .catch(err =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      })
+    );
+};
+
+// set logged in user
+export const setCurrentUser = decoded => {
+  return {
+    type: SET_CURRENT_USER,
+    payload: decoded
+  };
 };
