0777f0936efdbf9a7520cc3fb4c33cf57f1c332a
Riley-Brown <riley@riley.gg>
2018-12-16 22:54:19 -0500

    Added single profile component with sub components to display info for single users

---
+++ client/src/components/profile/Profile.js
@@ -0,0 +1,40 @@
+import React, { Component } from "react";
+import ProfileHeader from "./ProfileHeader";
+import ProfileAbout from "./ProfileAbout";
+import ProfileCreds from "./ProfileCreds";
+import ProfileGithub from "./ProfileGithub";
+import Spinner from "../common/Spinner";
+import { connect } from "react-redux";
+import { getProfileByHandle } from "../../actions/profileActions";
+import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
+
+class Profile extends Component {
+  componentDidMount() {
+    if (this.props.match.params.handle) {
+      this.props.getProfileByHandle(this.props.match.params.handle);
+    }
+  }
+  render() {
+    return (
+      <div>
+        <ProfileHeader />
+        <ProfileGithub />
+        <ProfileAbout />
+        <ProfileCreds />
+      </div>
+    );
+  }
+}
+
+Profile.propTypes = {
+  profile: PropTypes.object.isRequired,
+  getProfileByHandle: PropTypes.func.isRequired
+};
+const mapStateToProps = state => ({
+  profile: state.profile
+});
+export default connect(
+  mapStateToProps,
+  { getProfileByHandle }
+)(Profile);
