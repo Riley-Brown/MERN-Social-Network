ec8c5c385abc767d85d35d93e724f5d14afc5d12
Riley-Brown <riley@riley.gg>
2018-12-17 14:43:36 -0500

    Added functionality to display experience and education for single user profiles

--- client/src/components/profile/ProfileCreds.js
+++ client/src/components/profile/ProfileCreds.js
@@ -1,10 +1,92 @@
 import React, { Component } from "react";
+import Moment from "react-moment";
 
 class ProfileCreds extends Component {
   render() {
+    const { experience, education } = this.props;
+    const expItems = experience.map(exp => (
+      <li className="list-group-item" key={exp._id}>
+        <h4>{exp.company}</h4>
+        <p>
+          <Moment format="YYYY/MM/DD">{exp.from}</Moment> -
+          {exp.to === null ? (
+            " Now"
+          ) : (
+            <Moment format="YYYY/MM/DD">{exp.to}</Moment>
+          )}
+        </p>
+        <p>
+          <strong>Positon:</strong> {exp.title}
+        </p>
+        <p>
+          {exp.location === "" ? null : (
+            <span>
+              <strong>Location: </strong>
+              {exp.location}
+            </span>
+          )}
+        </p>
+        <p>
+          {exp.description === "" ? null : (
+            <span>
+              <strong>Description: </strong>
+              {exp.description}
+            </span>
+          )}
+        </p>
+      </li>
+    ));
+
+    const eduItems = education.map(edu => (
+      <li className="list-group-item" key={edu._id}>
+        <h4>{edu.school}</h4>
+        <p>
+          <Moment format="YYYY/MM/DD">{edu.from}</Moment> -
+          {edu.to === null ? (
+            " Now"
+          ) : (
+            <Moment format="YYYY/MM/DD">{edu.to}</Moment>
+          )}
+        </p>
+        <p>
+          <strong>Degree:</strong> {edu.degree}
+        </p>
+        <p>
+          {edu.location === "" ? null : (
+            <span>
+              <strong>Field of Study: </strong>
+              {edu.fieldofstudy}
+            </span>
+          )}
+        </p>
+        <p>
+          {edu.description === "" ? null : (
+            <span>
+              <strong>Description: </strong>
+              {edu.description}
+            </span>
+          )}
+        </p>
+      </li>
+    ));
     return (
-      <div>
-        <h1>Creds</h1>
+      <div className="row">
+        <div className="col-md-6">
+          <h3 className="text-center text-info">Experience</h3>
+          {expItems.length > 0 ? (
+            <ul className="list-group">{expItems}</ul>
+          ) : (
+            <p className="text-center">No experience listed</p>
+          )}
+        </div>
+        <div className="col-md-6">
+          <h3 className="text-center text-info">Education</h3>
+          {eduItems.length > 0 ? (
+            <ul className="list-group">{eduItems}</ul>
+          ) : (
+            <p className="text-center">No education listed</p>
+          )}
+        </div>
       </div>
     );
   }
